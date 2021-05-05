d6d48ca245c78f0df6985abe59af7b523e7b52ff
nathan <faustinenathaniel@gmail.com>
2021-05-05 20:57:39 +0700

    social media buttons

---
+++ src/components/footer/footer.component.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import ButtonRound from '../../components/button-round/button-round.component';
+import ship1 from '../../assets/ship-1-bubble.jpg';
+import { ReactComponent as PaperSVG } from '../../assets/paper-header.svg';
+import Amvlogo from '../../assets/logoamv-removebg.png';
+import './footer.styles.scss';
+import Bubble from '../bubble/bubble.component';
+import SocialMediaButtons from '../social-media-buttons/social-media-buttons';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer__logo">
+        {/* <img src={Amvlogo} alt="AMV LOGO"/> */}
+      </div>
+      
+      <div className="footer__socialmedia">
+        <p>Follow our social media</p>
+        <div className="footer__socialmedia__buttons">
+          <SocialMediaButtons />
+        </div>
+        
+      </div>
+
+      <div className="footer__copyright">
+        <p>Copyright @ 2021 AmvUI</p>
+        <p>Help and Support</p>
+        <p>roboboat.ui@gmail.com</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
